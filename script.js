1db02218639ecdca5bcf6ca64031cada5b83bf5f
timothyTaiHo <53063540+timothyTaiHo@users.noreply.github.com>
2019-07-18 15:48:25 -0700

    Added a JavaScript file

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleProjects(){
+var div = document.getElementById('projects');
+if(div.style.display == 'inherit'){
+div.style.display = 'none'
+}
+else {
+div.style.display = 'inherit'
+}
+}
